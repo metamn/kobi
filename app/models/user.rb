52f92f970234e8792d4bd0d5570526dfa736018c
rubycs <rubycs@clair.ro>
2010-02-20 18:33:28 +0200

    login name unique, dashboard flash message uses jquery to slide up

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,10 @@ class User < ActiveRecord::Base
          :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation
+  
+  # Validations
+  validates_presence_of :name
+  validates_uniqueness_of :name
+  validates_format_of :name, :with => /^[\d\w-]+$/i, :message => "Poate contine numai caracterele a-zA-Z0-9"
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,17 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+$(document).ready(function(){
+  
+  // Close flash messages on dashboard
+  $('#flash .close').click(function(event) {
+    $('#flash').slideUp('slow');
+  });
+  
+  // Close flash message on dashboard after 3 sec
+  $('#flash').delay(3000).slideUp('slow');
+  
+});
+
+
