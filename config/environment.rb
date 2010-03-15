ad4417976479dae1b0a6a6fc456c1f1053d3635a
rubycs <rubycs@clair.ro>
2010-03-15 22:09:36 +0200

    Semantic Menu replaced with Simple Navigation

--- config/environment.rb
+++ config/environment.rb
@@ -27,6 +27,7 @@ Rails::Initializer.run do |config|
   config.gem "acts-as-taggable-on"  
   config.gem 'validates_timeliness'
   #config.gem 'exceptional', :version => '2.0.6'
+  config.gem 'simple-navigation'
 
 
 
