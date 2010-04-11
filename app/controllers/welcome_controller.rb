297c68c40f81e8a47dcbbfa8209b0a192386fa51
rubycs <rubycs@clair.ro>
2010-04-11 19:30:21 +0300

    Welcome page designed, first approach

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,11 @@
 class WelcomeController < ApplicationController
   skip_before_filter :authenticate_user!
   
+  layout "welcome"
+  
+  
   def index    
-    redirect_to :controller => "sessions", :action => "new"    
+    #redirect_to :controller => "sessions", :action => "new"    
   end
 
 end
